3d4e012bbf0f6b4e001f49ea98d3dc435b0b5efb
vika <v.sapunkova@neurobotics.ru>
2020-05-06 09:18:02 +0300

    uncommented shouldTestErrorPhone

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SeleniumTest {
@@ -31,18 +29,6 @@ class SeleniumTest {
         driver.quit();
         driver = null;
     }
-
-//    @Test
-//    void shouldTestV1() {
-//        driver.get("http://localhost:9999");
-//        List<WebElement> elements = driver.findElements(By.className("input__control"));
-//        elements.get(0).sendKeys("Василий");
-//        elements.get(1).sendKeys("+79270000000");
-//        driver.findElement(By.className("checkbox__box")).click();
-//        driver.findElement(By.className("button")).click();
-//        String text = driver.findElement(By.className("alert-success")).getText();
-//        assertEquals("Ваша заявка успешно отправлена!", text.trim());
-//    }
 
     @Test
     void shouldTestSuccess() {
@@ -68,15 +54,15 @@ class SeleniumTest {
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", unsuccessText.trim());
     }
 
-//    @Test
-//    void shouldTestErrorPhone() {
-//        driver.get("http://localhost:9999");
-//        WebElement form = driver.findElement(By.cssSelector("form[class='form form_size_m form_theme_alfa-on-white']"));
-//        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-//        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("9270000000");
-//        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        form.findElement(By.cssSelector("span[class='button__text']")).click();
-//        String unsuccessText = driver.findElement(By.cssSelector("span[class='checkbox__text']")).getText();
-//        assertEquals("label[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']", unsuccessText.trim());
-//    }
+    @Test
+    void shouldTestErrorPhone() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("form[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("9270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("span[class='button__text']")).click();
+        String unsuccessText = driver.findElement(By.cssSelector("span[class='checkbox__text']")).getText();
+        assertEquals("label[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']", unsuccessText.trim());
+    }
 }
